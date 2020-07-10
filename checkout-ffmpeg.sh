abc2b48f391a031159cab2bf07b41bfe1d383f96
randomtree07 <ryanfeldbush@gmail.com>
2020-07-10 14:02:27 -0400

    Massive shift, ignore previous commits 	deleted:    ffmpeg/.gitattributes

---
+++ checkout-ffmpeg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+FFMPEG="ffmpeg-4.2"
+
+if [ ! -d "${dir}" ]; then
+    wget -q https://s3.amazonaws.com/ossimlabs/dependencies/source/$FFMPEG.tgz -O $FFMPEG.tgz
+    tar xvfz $FFMPEG.tgz
+    rm -f $FFMPEG.tgz
+fi
