c61fee365827713a46e1c85418436a6e603d953b
randomtree07 <ryanfeldbush@gmail.com>
2020-07-14 11:47:37 -0400

    updated directory names

--- checkout-x264.sh
+++ checkout-x264.sh
@@ -7,4 +7,5 @@ if [ ! -d "${dir}" ]; then
     wget -q https://s3.amazonaws.com/ossimlabs/dependencies/source/$X264.tgz -O $X264.tgz
     tar xvfz $X264.tgz
     rm -f $X264.tgz
+    mv $X264 x264
 fi
